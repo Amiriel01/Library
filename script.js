3254b231be5dda5f68f93f099fe15dd254067abd
Sarah Quinney <quinney.sarah@gmail.com>
2023-06-07 12:42:43 -0500

    toggle hidden form/table working

--- script.js
+++ script.js
@@ -1,35 +1,52 @@
 //Attach DOM objects. Link js to html//
 //main page//
-let addButton=document.querySelector("add-button");
-let table=document.querySelector(".table");
-let tableBody=document.querySelector("tbody");
+let addButton = document.querySelector("#add-button");
+let table = document.querySelector(".table");
+let tableBody = document.querySelector("tbody");
 //hidden form//
-let form=document.querySelector(".form");
-let title=form.querySelector("#book-title");
-let author=form.querySelector("#book-author");
-let pages=form.querySelector("#book-pages");
-let status=form.querySelector("#read-status");
-let back=form.querySelector("#back");
-let clear=form.querySelector("#clear");
-let submit=form.querySelector("#submit");
+let form = document.querySelector(".form");
+let title = form.querySelector("#book-title");
+let author = form.querySelector("#book-author");
+let pages = form.querySelector("#book-pages");
+let status = form.querySelector("#read-status");
+let opinion = form.querySelector("#read-opinion");
+let back = form.querySelector("#back");
+let clear = form.querySelector("#clear");
+let submit = form.querySelector("#submit");
 
 
 //creates empty array for books to go in//
 let myLibrary = [];
 
-/*function Book() {
-  // the constructor...
-}*/
-
-//this will create each book item in a div//
-function createBookItem(book,index) {
-    let bookItem=document.createElement("div")
+function Book(title, author, pages, status, opinion) {
+    //constructor. all books will have this data on the form and table//
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+    this.opinion = opinion;
 }
 
-//this will allow books to be added to the library//
-//.map creates a new array populated with the results of calling a function on every element in the calling array//
-function addBookToLibrary() {
-  myLibrary.map(function (book, index) {
-    createBookItem(book, index)
-  })
+//this will create books on the table from the form//
+function addBook() {
+    let title = title.value;
+    let author = author.value;
+    let pages = pages.value;
+    let status = status.value;
+    let opinion = opinion.value;
+    //this creates a new Book using the this parameters and pushes it to the myLibrary array//
+    let addNewBook = new Book(title, author, pages, status, opinion);
+    myLibrary.push(addNewBook);
 }
+
+//this will toggle the form and table using the add book button//
+//classList.toggle is one way to manipulate the read-only property that returns a live collection of the class attributes/elements//
+//hidden-form is found in html and css(display:none)//
+function toggleHidden() {
+    form.classList.toggle("hidden");
+    table.classList.toggle("hidden");
+    addButton.classList.toggle("hidden");
+}
+
+//this will make the add book button work//
+addButton.addEventListener("click", toggleHidden);
